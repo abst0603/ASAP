096fac2a77c22f53b0d597e75d91d4c03b4278e3
abst0603 <abolfazl.taghribi@gmail.com>
2020-10-14 16:25:12 +0200

    merging the random subsampling and packing condition in one loop, which make prog. faster

--- matlab/epsilon_dense.m
+++ matlab/epsilon_dense.m
@@ -1,11 +1,7 @@
 function samples = epsilon_dense(data,ep)
-samples = data;
 %% subsample all points randomly
-for i = 1:round(size(samples,1)/5)
-    aa = randperm(size(samples,1),1);
-    idx = rangesearch(samples,samples(aa,:),ep);
-    samples = setdiff(samples,samples(idx{1}(2:end),:),'rows','stable');
-end
+aa = randperm(size(data,1));
+samples = data(aa,:);
 cnt = 0;
 %check the packing condition
 while (cnt<size(samples,1))
